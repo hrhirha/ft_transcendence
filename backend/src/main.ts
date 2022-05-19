d272107c22bdbf7cb7ae7a2f03848ff9d3d4bc17
hrhirha <rhirha.hamza@gmail.com>
2022-05-19 16:07:01 +0100

    enabled CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.use(cookieParser());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
   await app.listen(3001);
 }
 bootstrap();
