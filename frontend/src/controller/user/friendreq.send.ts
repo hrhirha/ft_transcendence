fb52a1eedeb5116560306f9d537cb5dccf8b8944
wben-sai <wben-sai@student.1337.ma>
2022-05-20 16:23:56 +0100

    post_send_friends

--- frontend/src/controller/user/friendreq.send.ts
+++ frontend/src/controller/user/friendreq.send.ts
@@ -1,10 +1,9 @@
 import api from "../../api/axois";
 
-export async function post_friend_accept(user_id : string) {
-    // "friendreq/accept"
+export async function post_friend_send(user_id : string) {
+    // "friendreq/send"
     try {
         const res  = await api.post("contact", {id : user_id});
-        console.log(res);
         return res;
     }catch (err) {
         return new Error("error : " + err);
