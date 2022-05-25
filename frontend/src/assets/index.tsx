3ffe693601b6caf7135a2b3e7a697a0f15119d22
AimadBahdir <aimadbahdir.pro@gmail.com>
2022-05-25 19:58:24 +0100

    home cards

--- frontend/src/assets/index.tsx
+++ frontend/src/assets/index.tsx
@@ -1,3 +1,4 @@
 export {default as Logo42} from "./images/42logo.svg"
 export {default as Logo1337} from "./images/1337logo.svg"
 export {default as DefaultGame} from "./images/defaultgame.png"
+export {default as UltimateGame} from "./images/ultimategame.png"
