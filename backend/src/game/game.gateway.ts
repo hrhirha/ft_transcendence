3a35d037e49df2e1df1303993a38665dee336d56
Ismail Bouhiri <ibouhiri@e1r13p4.1337.ma>
2022-06-06 13:56:56 +0100

    New fonction before start logic

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -7,20 +7,29 @@ import {
     WebSocketServer,
     WsResponse 
 } from '@nestjs/websockets';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { ChatService } from 'src/chat/chat.service';
+import { UseGuards } from '@nestjs/common';
+import { Jwt2FAAuthGuard } from 'src/auth/guard/jwt-2fa-auth.guard';
 
-
+@UseGuards(Jwt2FAAuthGuard)
 @WebSocketGateway({ cors: true, namespace: 'game'})
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
 {
     @WebSocketServer()
     server : Server;
-
-    handleDisconnect(client: Socket) {
+ 
+    roomCreated: number = 0;
+    connection: any;
+ 
+    constructor(private prisma: PrismaService, private jwt: ChatService){}
+     handleDisconnect(client: Socket) {
        console.log('Client Disconnect with ID: ' + client.id);
     }
 
-    handleConnection(client: Socket, ...args: any[]) {
+    async handleConnection(client: Socket, ...args: any[]) {
         console.log('Client Connected with ID: ' + client.id);
+        this.beforeStart(client);
     }
 
     @SubscribeMessage('server')
@@ -30,5 +39,45 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect
             event: 'FromTheServerToClient', data: text
         };
         // this.server.emit('game', msg);
+    }
+
+    async beforeStart(client: Socket)
+    {
+        const user = (await this.jwt.getUserFromSocket(client));
+        if (!this.roomCreated)
+        {
+            this.connection =  await this.prisma.game.create({
+                data: {
+                    map: "map_url",
+                    user_game:
+                    {
+                        create: {
+                            uid: user.id,
+                            is_player: true,
+                            score: 0
+                        }
+                    }
+                },
+                select: {
+                    _count: true,
+                    id: true
+                }
+            });
+            this.roomCreated += 1;
+            client.join(this.connection.id);
+            return false;
+        }
+        await this.prisma.userGame.create({
+            data: {
+                uid: user.id,
+                is_player: true,
+                score: 0,
+                gid: this.connection.id,
+            },
+        });
+        client.join(this.connection.id);
+        this.server.to(this.connection.id).emit('startGame', this.connection.id);
+        this.roomCreated = 0;
+        return true;
     }
 }
--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ChatModule } from 'src/chat/chat.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { GameGateway } from './game.gateway';
 
 @Module({
-  imports: [],
+  imports: [ChatModule, PrismaModule],
   controllers: [],
   providers: [GameGateway],
 })
