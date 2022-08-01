3c7bbaa041fa9219809e1b6d5012e1745525e079
Aimad Bahdir <aimadbahdir.pro@gmail.com>
2022-08-01 13:07:11 +0100

    linked leader board

--- frontend/src/views/pages/leader_board/leader_board.tsx
+++ frontend/src/views/pages/leader_board/leader_board.tsx
@@ -1,17 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
-import {faMedal, faRankingStar, faTrophy } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import {faClose, faMedal, faRankingStar, faTrophy } from "@fortawesome/free-solid-svg-icons";
 import { CircleAvatar } from "../../components/circle_avatar/circle_avatar";
 import { Numeral } from "../../components/numeral/numeral";
+import { get_leader_board } from "../../../controller/leader_board/leader_board";
+import { useNavigate } from "react-router-dom";
+import { useNotif } from "../../components/notif/notif";
 
 interface RowProps {
     rank: number,
+    username: string,
     avatar: string,
     fullname: string,
     score: number
 }
 
 const BoardRow:React.FC<RowProps> = (RowProps) => {
+    const navigate = useNavigate();
     return(
     <tr id={"_"+RowProps.rank} className="col-sm-12 col-8">
         <td className="rank">
@@ -20,7 +25,7 @@ const BoardRow:React.FC<RowProps> = (RowProps) => {
             {RowProps.rank === 3 && <span><FontAwesomeIcon icon={faMedal}/></span>}
             {RowProps.rank > 3 && <span><Numeral value={RowProps.rank} /></span>}
         </td>
-        <td>
+        <td onClick={() => navigate(`/u/${RowProps.username}`)}>
             <CircleAvatar avatarURL={RowProps.avatar} dimensions={40} showStatus={false}/>
             <span className="fullName">{RowProps.fullname}</span>
         </td>
@@ -31,10 +36,26 @@ const BoardRow:React.FC<RowProps> = (RowProps) => {
 }
 
 export const LeaderBoard:React.FC = () => {
-    let users = [];
+    const [users, setUsers] = useState<Array<any>>();
+    const pushNotif = useNotif();
 
-    for (let i = 0; i < 100; i++)
-        users.push(<BoardRow key={i} rank={i + 1} avatar="https://i.stack.imgur.com/AVJwP.png" fullname="Jhon Doe" score={500 - i * 2}/>);
+    useEffect(() => {
+        (async () => {
+            try {
+                const usersList: Array<any> = await get_leader_board();
+                setUsers(usersList.map((u, k) => <BoardRow key={u.id} rank={k + 1} username={u.username} avatar={u.imageUrl} fullname={u.fullName} score={u.score}/>));
+            }
+            catch(e: any) {
+                pushNotif({
+                    id: "GETLEADERBOARDERROR",
+                    type: "error",
+                    icon: <FontAwesomeIcon icon={faClose}/>,
+                    title: "ERROR",
+                    description: e.message 
+                });
+            }
+        })();
+    }, []);
     return (
         <main id="leaderBoardPage">
             <div className='container'>
@@ -51,7 +72,7 @@ export const LeaderBoard:React.FC = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {users.map(e => e)}
+                        {users && users.map(e => e)}
                     </tbody>
                 </table>
             </div>
