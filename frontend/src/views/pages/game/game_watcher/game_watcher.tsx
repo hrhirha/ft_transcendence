2831feb366dd877b218631d9b9ba3b2441c29054
Aimad Bahdir <aimadbahdir.pro@gmail.com>
2022-08-12 21:50:28 +0100

    live games empty state

--- frontend/src/views/pages/game/game_watcher/game_watcher.tsx
+++ frontend/src/views/pages/game/game_watcher/game_watcher.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import { get_ongoing_matches, Match } from "controller/user/matches";
+import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { GameView } from "views/pages/game/game_view/game_view";
 
 export const GameWatcher:React.FC = () =>  {
     const socket = io("ws://127.0.0.1:3001/game", {withCredentials: true});
+    const [ongoingMatches, setMatches] = useState<Array<Match>>([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const _matches = await get_ongoing_matches();
+                setMatches(_matches);
+            } catch(e: any) {}
+        })();
+    },[]);
 
     return (
         <section id="gameWatcher">
-            
-            <GameView gameSocket={socket} isUltimate={false} watcher={true} roomId={"cl6qss5xm22476z6u29pgkqfqr"}/>
+            {ongoingMatches.length === 0 && <img className="nolivegames" />}
+            {ongoingMatches.length > 0 && <GameView gameSocket={socket} isUltimate={false} watcher={true} roomId={"cl6qss5xm22476z6u29pgkqfqr"}/>}
         </section>
     );
 }
